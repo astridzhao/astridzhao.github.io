071b55fd6a0a568bd8fae57d8e19133b79214ef8
AstridZhao <zhao26z@mtholyoke.edu>
2024-01-06 15:42:43 -0500

    fix error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import icon from "astro-icon";
-
-const localbuild_URL = "http://localhost:4321"
-const live_URL =  'https://astridzhao.github.io'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), icon()],
-  site: live_URL,
+  site: 'https://astridzhao.github.io',
 });
